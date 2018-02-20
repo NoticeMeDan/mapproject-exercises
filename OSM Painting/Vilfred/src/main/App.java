30ee0fe2c9035c30d6acedd413720f09d00c9657
Vilfred <Vilfredsikker@gmail.com>
2018-02-20 21:47:23 +0100

    Main through "App" instead og "View"

--- OSM Painting/Vilfred/src/main/App.java
+++ OSM Painting/Vilfred/src/main/App.java
@@ -1,9 +1,22 @@
 package main;
 
-import main.view.view;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
     public static void main(String[] args) {
-        view.launch(null);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/fxml/ui.fxml"));
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setTitle("OSM Painting");
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
